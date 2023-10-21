e43837e6adeb9d9cdfb2e7569527481227291c3b
lie03 <139715955+lie031@users.noreply.github.com>
2023-10-21 12:08:23 -0500

    complete 1-4 exercises

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function(text) {
+    let reverseText = "";
+    for(let i=0; i<= text.length ; i++ ){
+     reverseText += text.charAt(text.length-i);
+    }
+    return reverseText;   
 };
 
 // Do not edit below this line
--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,5 @@
-const removeFromArray = function() {
-
-};
-
+const removeFromArray = function(Arr, ...args) {
+    return Arr.filter(item => !args.includes(item));
+}
 // Do not edit below this line
 module.exports = removeFromArray;
