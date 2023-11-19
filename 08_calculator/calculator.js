fa47faa32a1cf5268eca46bb2a451f0122418a4a
lie03 <139715955+lie031@users.noreply.github.com>
2023-11-19 14:42:58 -0500

    all exercises done

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,26 @@
-const add = function() {
-	
+const add = function(num1,num2) {
+	return num1 + num2;
 };
 
-const subtract = function() {
-	
+const subtract = function(num1,num2) {
+	return num1-num2;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+	 return array.reduce((sum,element)=> sum+element,0);
+}
+
+const multiply = function(array) {
+  return array.reduce((product,element)=> product*element,1)
 };
 
-const multiply = function() {
-
+const power = function(base,exp) {
+  return base**exp;
 };
 
-const power = function() {
-	
-};
-
-const factorial = function() {
-	
+const factorial = function(num) {
+  if(num===0)return 1;
+  return num * factorial(num-1);
 };
 
 // Do not edit below this line
